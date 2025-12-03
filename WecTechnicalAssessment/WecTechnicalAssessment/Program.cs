3f3f580ddec85bf067aa357f822e91bab84ef05e
farrukhkhan89 <43439998+farrukhkhan89@users.noreply.github.com>
2025-12-03 18:17:53 +0500

    Final tasks

--- WecTechnicalAssessment/WecTechnicalAssessment/Program.cs
+++ WecTechnicalAssessment/WecTechnicalAssessment/Program.cs
@@ -81,5 +81,52 @@ namespace WecTechnicalAssessment
             return sb.ToString();
         }
 
+        public List<long> GenerateFibonacci(int limit)
+        {
+            List<long> result = new List<long>();
+
+            long a = 0, b = 1;
+
+            while (a <= limit)
+            {
+                result.Add(a);
+                long next = a + b;
+                a = b;
+                b = next;
+            }
+
+            return result;
+        }
+
+        public List<string> FizzBuzzFibonacci(long limit)
+        {
+            List<string> result = new List<string>();
+
+            long a = 0, b = 1;
+
+            while (a <= limit)
+            {
+                string output = "";
+
+                if (a % 3 == 0 && a % 5 == 0 && a != 0)
+                    output = "FizzBuzz";
+                else if (a % 3 == 0 && a != 0)
+                    output = "Fizz";
+                else if (a % 5 == 0 && a != 0)
+                    output = "Buzz";
+                else
+                    output = a.ToString();
+
+                result.Add(output);
+
+                long next = a + b;
+                a = b;
+                b = next;
+            }
+
+            return result;
+        }
+
+
     }
 }
